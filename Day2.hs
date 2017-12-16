a5bf2fb92bb800e7c7abd3949f88689b020b3478
Adrian Nilsson <nadrian@student.chalmers.se>
2017-12-16 17:26:35 +0100

    Add solution to second puzzle of day 2
    Also generalized the code to not repeat the same patterns from the first puzzle.

--- Day2.hs
+++ Day2.hs
@@ -1,17 +1,57 @@
 import Data.Maybe
 import Text.Read (readMaybe)
-import Control.Monad.Trans
-import Control.Monad.Trans.Maybe
+
 
 main = interact linefun
 
+--------------------- Part 1 -------------------- 
+
+part1 :: [Int] -> Int
+part1 = checksum1
+
+checksum1 :: [Int] -> Int
+checksum1 xs = (maximum xs) - (minimum xs) 
+
+
+--------------------- Part 2 -------------------- 
+
+part2 :: [Int] -> Int
+part2 = sum . checksum2        
+
+{- | A element is replaced by a zero if it had no divisors, 
+     otherwise it will be replaced with the resulting division -}
+checksum2 :: [Int]  -- ^ Assume no duplicates and at most one divisor per elem.
+          -> [Int]  
+checksum2 [] = []
+checksum2 (x:xs) = (checksum2' filtered x : checksum2 xs)
+  where filtered = filter (isEvenlyDiv x) xs 
+
+checksum2' :: [Int] -> Int -> Int
+checksum2' [] _ = 0 
+checksum2' (x:_) y = (max x y) `div` (min x y)
+
+
+-- Filter function 
+isEvenlyDiv :: Int -> Int -> Bool
+isEvenlyDiv x y = isEvenlyDiv' modulo
+  where modulo = (max x y) `mod` (min x y)
+
+isEvenlyDiv' :: Int -> Bool
+isEvenlyDiv' 0 = True 
+isEvenlyDiv' _ = False
+
+
+----------------- Shared Functions --------------
+
+-- Transform the intput to an output string
 linefun :: String -> String
-linefun x = show ch
+linefun x = (show ch) ++ "\n"
+  where xs = parseInput x
+        ch = sum $ map part2 xs -- choose part1 or part2
+
+
+parseInput :: String -> [[Int]] 
+parseInput x = map parseInt xs
   where xs = (map words . lines) x
-        ys = map (mapMaybe (readMaybe :: String -> Maybe Int)) xs
-        ch = sum $ map checksum ys
-
-
-checksum :: [Int] -> Int
-checksum xs = (maximum xs) - (minimum xs) 
+        parseInt = (mapMaybe (readMaybe :: String -> Maybe Int)) 
 
